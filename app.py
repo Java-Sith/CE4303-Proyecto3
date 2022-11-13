b2a223dc60df9b910141df0bc122f410d468927c
Java-Sith <prieto.luisdaniel@gmail.com>
2022-11-13 15:52:27 -0600

    Se añaden HTML, CSS y requests

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, redirect
 
 app = Flask(__name__)
+
+""" songs = {{}}
+
+song = {'Song': 'Somebody To Love', 'Author': 'Freddie Mercury', 'Group': 'Queen'}
+
+songs[0] = song
+
+i = 0  """
+
+songs = []
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        pass
+        song_content = request.form['content']
+        try:
+            songs.append(song_content)
+            print(songs)
+            return redirect('/')
+        except:
+            return "There was an issue posting!" + str(song_content)
     else:
-        pass
+        return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
