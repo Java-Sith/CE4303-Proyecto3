7532ca3bf515eceacab8962671cc976adcd553a7
Java-Sith <prieto.luisdaniel@gmail.com>
2022-11-15 18:38:00 -0600

    Se integra con XML Tree

--- app.py
+++ app.py
@@ -1,67 +1,107 @@
 from flask import Flask, render_template, request, redirect
 import xml.etree.ElementTree as ET
-
+import time as T
 
 app = Flask(__name__)
 
-songs = { }
-
-i = 0
-
-# song = {'Song': 'Somebody To Love', 'Author': 'Freddie Mercury', 'Group': 'Queen'}
-
-# songs[0] = song
-
-# print(songs)
+tree = ET.parse('Queen.xml')
+root = tree.getroot()
 
 def xml_injection(id, count):
+    start = T.time()
+    cancionFavorita = ET.Element('CancionFavorita')
+    titulo = ET.Element('Titulo') 
+    autor = ET.Element('Autor') 
+    grupo = ET.Element('Grupo') 
     if id == 1:
-        return render_template('index.html', songs = songs)
+        while count < 100000:
+            titulo.text = 'Bohemian Rhapsody'
+            autor.text = 'Freddie Mercury'
+            grupo.text = 'Queen'
+            titulo.tail = "\n    "
+            autor.tail = "\n    "
+            grupo.tail = "\n    "
+            cancionFavorita.append(titulo)
+            cancionFavorita.append(autor)
+            cancionFavorita.append(grupo)
+            cancionFavorita.tail = "\n    "
+            root.append(cancionFavorita)
+            root.set('id', 1)
+            count += 1
+            tree.write('Queen.xml')
+        end = T.time()
+        print(end - start)
+        return render_template('index.html', root = root)
     else:
-        while count < 10000:
-            songs[count] = {'Song': 'Somebody To Love', 'Author': 'Freddie Mercury', 'Group': 'Queen'}
-        return render_template('index.html', songs = songs)
+        return render_template('index.html', root = root)
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    global i
+    #POST
     if request.method == 'POST':
-        song_name = request.form['song']
-        song_author = request.form['author']
-        song_group = request.form['group']
+        start = T.time()
+        cancionFavorita = ET.Element('CancionFavorita')
+        titulo = ET.Element('Titulo') 
+        autor = ET.Element('Autor') 
+        grupo = ET.Element('Grupo') 
         try:
-            songs[i] = {'Song': str(song_name), 'Author': str(song_author), 'Group': str(song_group)}
-            i += 1
-            print(songs)
-            print(i)
+            titulo.text = request.form['song']
+            autor.text = request.form['author']
+            grupo.text = request.form['group']
+            titulo.tail = "\n    "
+            autor.tail = "\n    "
+            grupo.tail = "\n    "
+            cancionFavorita.append(titulo)
+            cancionFavorita.append(autor)
+            cancionFavorita.append(grupo)
+            cancionFavorita.tail = "\n    "
+            root.append(cancionFavorita)
+            root.set('id', request.form['id'])
+            tree.write('Queen.xml')
+            end = T.time()
+            print(end - start)
             return redirect(('/'))
         except:
+            end = T.time()
+            print(end - start)
             return "There was an issue posting!"
+    #GET
     else:
-        return xml_injection(1, 0)
-
-@app.route('/delete/<int:id>')
-def delete(id):
-    try:
-        songs[id] = { }
-        print(songs)
-        return redirect(('/'))
-    except:
-        return "There was an issue deleting!"
+        return xml_injection(request.form['id'], 0)
 
 @app.route('/update/<int:id>', methods=['POST', 'GET'])
 def update(id):
     if request.method == 'POST':
-        song_name = request.form['song']
-        song_author = request.form['author']
-        song_group = request.form['group']
+        start = T.time()
+        cancionFavorita = ET.Element('CancionFavorita')
+        titulo = ET.Element('Titulo') 
+        autor = ET.Element('Autor') 
+        grupo = ET.Element('Grupo') 
+        cancionFavorita = ET.Element('CancionFavorita')
+        titulo = ET.Element('Titulo') 
+        autor = ET.Element('Autor') 
+        grupo = ET.Element('Grupo') 
         try:
-            songs[id] = {'Song': str(song_name), 'Author': str(song_author), 'Group': str(song_group)}
+            titulo.text = request.form['song']
+            autor.text = request.form['author']
+            grupo.text = request.form['group']
+            titulo.tail = "\n    "
+            autor.tail = "\n    "
+            grupo.tail = "\n    "
+            cancionFavorita.insert(0, titulo)
+            cancionFavorita.insert(1, autor)
+            cancionFavorita.insert(2, grupo)
+            cancionFavorita.tail = "\n    "
+            root.insert(id, cancionFavorita)
+            end = T.time()
+            print(end - start)
             return redirect(('/'))
         except:
+            end = T.time()
+            print(end - start)
             return "There was an issue posting!"
     else:
-        return render_template('update.html', songs = songs)
+        return xml_injection(0, 0)
 
 if __name__ == "__main__":
     app.run(debug=True)
