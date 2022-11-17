def24c7490b5b6d8a96c51b70c4b00030c4778a9
Java-Sith <prieto.luisdaniel@gmail.com>
2022-11-17 14:51:42 -0600

    Se restaura el otro ejemplo

--- App_Aux/app.py
+++ App_Aux/app.py
@@ -1,114 +1,50 @@
-from flask import Flask, render_template, request, redirect
-import xml.etree.ElementTree as ET
 import time as T
+from lxml import etree as ET
+from xml.dom.minidom import parse
+import xmltodict as X
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
+time = 0
+songs = { }
 
-tree = ET.parse('Queen.xml')
-root = tree.getroot()
-id = 0
+def getText(nodeList):
+    songs = { }
+    i = 0
+    for node in nodeList['Musica']:
+        songs[i] = {'Titulo': node['CancionFavorita']['Titulo'], 'Autor': node['CancionFavorita']['Autor'], 'Grupo': node['CancionFavorita']['Grupo']}
+        i += 1
+    return songs
 
-def xml_injection(id, count, time):
-    start = T.time()
-    cancionFavorita = ET.Element('CancionFavorita')
-    cancionFavorita.tail = "\n"
-    cancionFavorita.text = "\n\t\t" 
-    titulo = ET.SubElement(cancionFavorita, 'Titulo') 
-    autor = ET.SubElement(cancionFavorita, 'Autor') 
-    grupo = ET.SubElement(cancionFavorita, 'Grupo') 
-    titulo.tail = "\n\t\t"
-    autor.tail = "\n\t\t"
-    grupo.tail = "\n\t\t"
-    print("Va por acá")
-    if id == "1":
-        while count < 300:
-            titulo.text = 'Bohemian Rhapsody'
-            autor.text = 'Freddie Mercury'
-            grupo.text = 'Queen' 
-            cancionFavorita.append(titulo)
-            cancionFavorita.append(autor)
-            cancionFavorita.append(grupo)
-            root[-1].tail = "\n\t"
-            root.append(cancionFavorita)
-            root.set('id', id)
-            count += 1
-            tree.write('Queen.xml')
+@app.route("/", methods=['GET', 'POST'])
+def parse_xml():
+    global songs, time
+    if request.method == 'POST':
+        start = T.time()
+        xml = request.files['file']
+        #xml_data = parse(xml)
+        #buffer = io.BytesIO()
+        parser = ET.XMLParser(dtd_validation=True)
+        xml_data = ET.parse(xml, parser=parser)
+        song = X.parse(xml_data)
+        print(jsonify(song))
+        songs = getText(song)
         end = T.time()
         time = end - start
         print(time)
-        return render_template('index.html', root = root, time = time)
+        return render_template('index.html', time = time, songs = songs)
+        #return xml_data.version
     else:
-        return render_template('index.html', root = root, time = time)
+        return render_template('index.html', time = time, songs = songs)
 
-@app.route('/', methods=['POST', 'GET'])
-def index():
-    time = 0
-    global id
-    #POST
-    if request.method == 'POST':
-        start = T.time()
-        cancionFavorita = ET.Element('CancionFavorita')
-        cancionFavorita.tail = "\n"
-        cancionFavorita.text = "\n\t\t" 
-        titulo = ET.SubElement(cancionFavorita, 'Titulo') 
-        autor = ET.SubElement(cancionFavorita, 'Autor') 
-        grupo = ET.SubElement(cancionFavorita, 'Grupo') 
-        titulo.tail = "\n\t\t"
-        autor.tail = "\n\t\t"
-        grupo.tail = "\n\t\t"
-        try:
-            titulo.text = request.form['song']
-            autor.text = request.form['author']
-            grupo.text = request.form['group']
-            root[-1].tail = "\n\t"
-            root.append(cancionFavorita)
-            root.set('id', request.form['id'])
-            tree.write('Queen.xml')
-            id = request.form['id']
-            end = T.time()
-            time = end - start
-            print(time)
-            return redirect(('/'))
-        except:
-            end = T.time()
-            time = end - start
-            print(time)
-            return "There was an issue posting!"
-    #GET
-    else:
-        return xml_injection(id, 0, time)
-
-@app.route('/update/<int:id>', methods=['POST', 'GET'])
-def update(id):
-    time = 0
-    if request.method == 'POST':
-        start = T.time()
-        cancionFavorita = ET.Element('CancionFavorita')
-        cancionFavorita.tail = "\n"
-        cancionFavorita.text = "\n\t\t" 
-        titulo = ET.SubElement(cancionFavorita, 'Titulo') 
-        autor = ET.SubElement(cancionFavorita,'Autor') 
-        grupo = ET.SubElement(cancionFavorita, 'Grupo') 
-        titulo.tail = "\n\t\t"
-        autor.tail = "\n\t\t"
-        grupo.tail = "\n\t\t"
-        try:
-            titulo.text = request.form['song']
-            autor.text = request.form['author']
-            grupo.text = request.form['group'] 
-            root[-1].tail = "\n\t"
-            root.insert(id, cancionFavorita)
-            end = T.time()
-            time = end - start
-            print(time)
-            return redirect(('/'))
-        except:
-            end = T.time()
-            time = end - start
-            print(time)
-            return "There was an issue posting!"
-    else:
-        return xml_injection(id, 0, time)
-
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    print("Starting python app")
+    # xml_data = parse('Queen.xml')
+    # song = xml_data.getElementsByTagName('CancionFavorita')
+    # songs = getText(song)
+    # print(songs)
+    # for key, song in songs.items():
+    #     print(song['Titulo'])
+    #     print(song['Autor'])
+    #     print(song['Grupo'])
+    app.run(host='0.0.0.0', port=8080, debug=True)
